f71394703c1e3d48d2f48a05c46780faa5897784
James Souksavanh Moua <Smoua1558@gmail.com>
2020-02-11 12:50:23 -0500

    Created max function and min function with while-loop instead of for-loop

--- HannahJamesPair1.js
+++ HannahJamesPair1.js
@@ -8,3 +8,50 @@ function average(array){
   return sum/array.length;
 }
 console.log(average([1,2,3,4]));
+
+
+//min-max function
+
+function max(numbers) {
+  let i = 0;
+
+  if (numbers.length === 0) {
+    return undefined;
+  }
+
+  let placeholder = numbers[0];
+
+  while (i < numbers.length) {
+    if (numbers[i] > placeholder) {
+      placeholder = numbers[i];
+    }
+
+    i++;
+  }
+
+  return placeholder;
+}
+
+console.log(max([8,16,12,4,5,24,3,27]));
+
+function min(numbers) {
+  let i = 0;
+
+  if (numbers.length === 0) {
+    return undefined;
+  }
+
+  let placeholder = numbers[0];
+
+  while (i < numbers.length) {
+    if (numbers[i] < placeholder) {
+      placeholder = numbers[i];
+    }
+
+    i++;
+  }
+
+  return placeholder;
+}
+
+console.log(min([3,2,4,1,15,8,6,32,9]));
